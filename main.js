23332aa6195f689ac2af99fed363ae628b1019dc
SAMS19-CDM <samscock731@gmail.com>
2024-11-25 13:44:21 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Ejemplo de función que puedes ejecutar cuando se hace clic en un botón
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', function() {
-        console.log(`Se hizo clic en el botón: ${this.innerHTML}`);
-        // Puedes agregar más funcionalidad aquí, como redirigir o mostrar un mensaje
-    });
-});
